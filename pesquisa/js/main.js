7a0ebbaa68a97c0aa0894cbe94fdf75b4e3e76e1
VGS <gs.launchdrive@gmail.com>
2023-06-03 14:54:59 -0300

    pagina pesquisa

---
+++ pesquisa/js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+
+    function redirectWhatsApp() {
+        setTimeout(function () {
+            window.location.href = 'https://forms.gle/FQtKZffrSt9DrpHn6';
+        }, 500);
+    }
+
+    redirectWhatsApp();
+});
